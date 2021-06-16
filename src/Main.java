b1c6e0fd7d1dd95ca34cc6b3550bdfc75e739413
Vicen <85416824+Vicensk@users.noreply.github.com>
2021-06-16 08:07:20 -0700

    right now it only has the checker

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+package me.vicensk.doubledrops;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("Hello Double Drops!");
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
